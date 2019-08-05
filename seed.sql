78b65207239dc72be09a7aa7f7ed1649cffe7024
kach92 <kenny.ang9206@gmail.com>
2019-08-05 15:49:14 +0800

    added seed file

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (name,password) VALUES ("Kenny Ang","f8780dd0a49d7c9b3bd3ec75cb3b80bb786f123442fde41c407eaa2e62e141c5");
-INSERT INTO users (name,password) VALUES ("Akira","af1a2068f1e68daa761b0a70ba8e4adc43bb258831536d49091b9904ebdd7367");
-INSERT INTO users (name,password) VALUES ("Khai","64fb4486d5941521274028a721d250632ffe6b2f8a86b2c08aecf01d4ab0e424");
-INSERT INTO users (name,password) VALUES ("Herda","6585bda252b873ac8367ac129d54f56d0448447f11a7dce8895d3446db2393e3");
+INSERT INTO users (name,password,mobile) VALUES ("Kenny Ang","f8780dd0a49d7c9b3bd3ec75cb3b80bb786f123442fde41c407eaa2e62e141c5","83180407");
+INSERT INTO users (name,password,mobile) VALUES ("Akira","af1a2068f1e68daa761b0a70ba8e4adc43bb258831536d49091b9904ebdd7367","88888888");
+INSERT INTO users (name,password,mobile) VALUES ("Khai","64fb4486d5941521274028a721d250632ffe6b2f8a86b2c08aecf01d4ab0e424","84133296");
+INSERT INTO users (name,password,mobile) VALUES ("Herda","6585bda252b873ac8367ac129d54f56d0448447f11a7dce8895d3446db2393e3","95746283");
